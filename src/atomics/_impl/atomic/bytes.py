9ccfef99ffe4be757239873fbe14eceee999946e
doodspav <doodspav@gmail.com>
2021-11-02 23:07:55 +0000

    Remade classes in `int.py` and `bytes.py` to use new mixins
    - also removed `baseint.py`
    - everything in it is now provided by new mixins

--- src/atomics/_impl/atomic/bytes.py
+++ src/atomics/_impl/atomic/bytes.py
@@ -1,14 +1,28 @@
-from .base import AtomicBase, AtomicViewBase
+from .base import AtomicBase, AtomicViewBase, AtomicViewBaseContext
+from .core import AtomicCore
+
 from .mixins.byteops import ByteOperationsMixin
+from .mixins.properties import BytePropertiesMixin
 
 
-class AtomicBytes(AtomicBase, ByteOperationsMixin):
+class AtomicBytes(AtomicBase, ByteOperationsMixin, BytePropertiesMixin):
 
     def __init__(self, *, width: int):
         super().__init__(width=width, is_integral=False, is_signed=False)
 
 
-class AtomicBytesView(AtomicViewBase, ByteOperationsMixin):
+class AtomicBytesView(AtomicViewBase, ByteOperationsMixin, BytePropertiesMixin):
+
+    def __init__(self, core: AtomicCore):
+        super().__init__(core)
+
+
+class AtomicBytesViewContext(AtomicViewBaseContext):
 
     def __init__(self, *, buffer):
         super().__init__(buffer=buffer, is_integral=False, is_signed=False)
+
+    def __enter__(self) -> AtomicBytesView:
+        self._assert_enter_preconditions()
+        self._entered = True
+        return AtomicBytesView(self._core)
--- src/atomics/_impl/atomic/int.py
+++ src/atomics/_impl/atomic/int.py
@@ -1,26 +1,51 @@
-from .baseint import AtomicIntegralBase, AtomicIntegralViewBase
+from .base import AtomicBase, AtomicViewBase, AtomicViewBaseContext
+from .core import AtomicCore
+
 from .mixins.intops import IntegralOperationsMixin
+from .mixins.properties import IntegralPropertiesMixin
 
 
-class AtomicInt(AtomicIntegralBase, IntegralOperationsMixin):
-
-    def __init__(self, *, width: int):
-        super().__init__(width=width, is_signed=True)
-
-
-class AtomicUint(AtomicIntegralBase, IntegralOperationsMixin):
+class AtomicInt(AtomicBase, IntegralOperationsMixin, IntegralPropertiesMixin):
 
     def __init__(self, *, width: int):
-        super().__init__(width=width, is_signed=False)
+        super().__init__(width=width, is_integral=True, is_signed=True)
 
 
-class AtomicIntView(AtomicIntegralViewBase, IntegralOperationsMixin):
+class AtomicIntView(AtomicViewBase, IntegralOperationsMixin, IntegralPropertiesMixin):
+
+    def __init__(self, core: AtomicCore):
+        super().__init__(core)
+
+
+class AtomicIntViewContext(AtomicViewBaseContext):
 
     def __init__(self, *, buffer):
-        super().__init__(buffer=buffer, is_signed=True)
+        super().__init__(buffer=buffer, is_integral=True, is_signed=True)
+
+    def __enter__(self) -> AtomicIntView:
+        self._assert_enter_preconditions()
+        self._entered = True
+        return AtomicIntView(self._core)
 
 
-class AtomicUintView(AtomicIntegralViewBase, IntegralOperationsMixin):
+class AtomicUint(AtomicBase, IntegralOperationsMixin, IntegralPropertiesMixin):
+
+    def __init__(self, *, width: int):
+        super().__init__(width=width, is_integral=True, is_signed=False)
+
+
+class AtomicUintView(AtomicViewBase, IntegralOperationsMixin, IntegralPropertiesMixin):
+
+    def __init__(self, core: AtomicCore):
+        super().__init__(core)
+
+
+class AtomicUintViewContext(AtomicViewBaseContext):
 
     def __init__(self, *, buffer):
-        super().__init__(buffer=buffer, is_signed=True)
+        super().__init__(buffer=buffer, is_integral=True, is_signed=False)
+
+    def __enter__(self) -> AtomicUintView:
+        self._assert_enter_preconditions()
+        self._entered = True
+        return AtomicUintView(self._core)

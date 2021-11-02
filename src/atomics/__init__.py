56d37cccf7717846a7c359621682c1a9b8eedfcf
doodspav <doodspav@gmail.com>
2021-11-02 23:09:58 +0000

    Updated `__init__.py` with new classes

--- src/atomics/__init__.py
+++ src/atomics/__init__.py
@@ -2,12 +2,14 @@ from ._impl.alignment import Alignment
 from ._impl.enums import MemoryOrder, OpType
 from ._impl.exceptions import AlignmentError, MemoryOrderError, UnsupportedWidthException, UnsupportedOperationException
 
-from ._impl.atomic.bytes import AtomicBytes, AtomicBytesView
+from ._impl.atomic.bytes import AtomicBytes, AtomicBytesView, AtomicBytesViewContext
 from ._impl.atomic.int import AtomicInt, AtomicUint, AtomicIntView, AtomicUintView
+from ._impl.atomic.int import AtomicIntViewContext, AtomicUintViewContext
 
 
 __all__ = ["AtomicBytes", "AtomicInt", "AtomicUint",
            "AtomicBytesView", "AtomicIntView", "AtomicUintView",
+           "AtomicBytesViewContext", "AtomicIntViewContext", "AtomicUintViewContext",
            "Alignment", "MemoryOrder", "OpType",
            "AlignmentError", "MemoryOrderError",
            "UnsupportedWidthException", "UnsupportedOperationException"]

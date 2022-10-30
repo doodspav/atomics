853c1e4e8f1f948499f005402b558c3f6145d609
doodspav <doodspav@gmail.com>
2022-10-30 16:16:21 +0000

    Add version checking at setup/compile time

--- setup.py
+++ setup.py
@@ -1,18 +1,54 @@
-from pathlib import Path
+import pathlib
+import tempfile
+
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 
-include_dir = Path.cwd().joinpath("ext/patomic/installdir/include").absolute()
-library_dir = Path.cwd().joinpath("ext/patomic/installdir/lib").absolute()
 
-atomics = Extension(
-    name="atomics",
-    sources=["src/atomics.pyx"],
-    include_dirs=[str(include_dir)],
-    library_dirs=[str(library_dir)],
-    libraries=["patomic"]
-)
+# required patomic versions
+patomic_version_major = 0
+patomic_version_minor = 5
+patomic_version_patch = 1
 
-setup(
-    ext_modules=cythonize(atomics, language_level=3)
-)
+
+# currently version is required to match exactly
+cython_patomic_version_check = f"""
+cdef extern from *: 
+    \"""
+    #include <patomic/patomic_version.h>
+    
+    #if PATOMIC_VERSION_MAJOR != {patomic_version_major}
+        #error Incompatible patomic major version. Required: =={patomic_version_major}.
+    #elif PATOMIC_VERSION_MINOR != {patomic_version_minor}
+        #error Incompatible patomic minor version. Required: =={patomic_version_minor}.
+    #elif PATOMIC_VERSION_PATCH != {patomic_version_patch}
+        #error Incompatible patomic patch version. Required: =={patomic_version_patch}.
+    #endif
+    \"""
+"""
+
+
+# installed patomic paths
+include_dir = pathlib.Path.cwd().joinpath("ext/patomic/installdir/include").absolute()
+library_dir = pathlib.Path.cwd().joinpath("ext/patomic/installdir/lib").absolute()
+
+
+with tempfile.TemporaryDirectory(prefix="__cython") as td:
+
+    name = "patomic_version_check.pyx"
+    path = pathlib.Path(td).joinpath(name)
+
+    with open(path, "w+") as f:
+        f.write(cython_patomic_version_check)
+
+    atomics = Extension(
+        name="atomics",
+        sources=["src/atomics.pyx", str(path)],
+        include_dirs=[str(include_dir)],
+        library_dirs=[str(library_dir)],
+        libraries=["patomic"]
+    )
+
+    setup(
+        ext_modules=cythonize(atomics, language_level=3)
+    )

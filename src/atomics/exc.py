1653a4c71a1bb9da8e55999ef3f0787f64fce106
doodspav <doodspav@gmail.com>
2022-11-24 15:40:09 +0000

    Add exceptions

---
+++ src/atomics/exc.py
@@ -0,0 +1,8 @@
+from ._atomics.atomics import AlignmentError, MemoryOrderError, UnsupportedOperationException, UnsupportedWidthException
+
+__all__ = [
+    "AlignmentError",
+    "MemoryOrderError",
+    "UnsupportedOperationException",
+    "UnsupportedWidthException"
+]

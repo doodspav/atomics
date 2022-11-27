cd93201c115e3645eb3ad0df2f906b0a267191a7
doodspav <doodspav@gmail.com>
2022-11-27 22:45:29 +0000

    Add exceptions

--- src/atomics/_atomics/atomics.pyx
+++ src/atomics/_atomics/atomics.pyx
@@ -1,4 +1,7 @@
 from . cimport version_check
 
 from .enums cimport _MemoryOrder, _OpType
+
 from .enums import MemoryOrder, OpType
+from .exceptions import AlignmentError, MemoryOrderError
+from .exceptions import UnsupportedOperationException, UnsupportedWidthException
